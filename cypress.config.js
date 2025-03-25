b4e0438de9e6cb59d12a3a5c46f25b8158039172
alexisklaser <klaser403@gmail.com>
2025-03-25 15:08:42 -0300

    Alterando README do projeto.

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,5 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   viewportHeight: 880,
   viewportWidth: 1280,
-  e2e: {},
-  video: true //salva um video da execução do teste na pasta e2e/videos
+  e2e: {}
 })
